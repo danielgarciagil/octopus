0d4b4f224c7a4b72819c95bf6dd0fac017dfecce
Daniel Garcia-Gil <danielgarciagil@gmail.com>
2022-01-12 15:48:17 -0400

    soporte para rails 6

--- lib/octopus/collection_association.rb
+++ lib/octopus/collection_association.rb
@@ -1,14 +1,13 @@
 module Octopus
   module CollectionAssociation
     def self.included(base)
-      if Octopus.rails51? || Octopus.rails52?
+      if Octopus.rails51? || Octopus.rails52? || Octopus.atleast_rails52? 
         base.sharded_methods :reader, :writer, :ids_reader, :ids_writer, :create, :create!,
                              :build, :include?,
                              :load_target, :reload, :size, :select
       else
         base.sharded_methods :reader, :writer, :ids_reader, :ids_writer, :create, :create!,
-                             :build, :any?, :count, :empty?, :first, :include?, :last, :length,
-                             :load_target, :many?, :reload, :size, :select, :uniq
+                             :build, :empty?, :include?, :load_target, :reload, :size, :select
       end
     end
   end
